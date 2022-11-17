1b47f85a3dbf67d27a264248b325a7b25d674bdd
Phil Zona <pzona@launchdarkly.com>
2022-11-17 10:30:39 -0500

    Add TS to project (now broken due to TS errors)

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-module.exports = dotpath
-
-function dotpath (str) {
-  var parts = str.toString().split('.')
-  var len = parts.length
-
-  return function parse (obj) {
-    var testKey
-
-    for (var i = 0; i < len; ++i) {
-      testKey = parts[i]
-
-      if (!obj) return
-
-      obj = obj[testKey]
-    }
-
-    return obj
-  }
+"use strict";
+module.exports = dotpath;
+function dotpath(str) {
+    var parts = str.toString().split('.');
+    var len = parts.length;
+    return function parse(obj) {
+        var testKey;
+        for (var i = 0; i < len; ++i) {
+            testKey = parts[i];
+            if (!obj)
+                return;
+            obj = obj[testKey];
+        }
+        return obj;
+    };
 }
